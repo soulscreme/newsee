79d2db6ad8b58f2f7658c7203350da6ef482438b
LJ Katz <soulscreme@gmail.com>
2013-04-05 15:49:20 -0400

    Fixed scala-reflect missing from POM.

--- src/main/scala/com/soulmedia/aggrigator/StoryHandlerWorker.scala
+++ src/main/scala/com/soulmedia/aggrigator/StoryHandlerWorker.scala
@@ -1,7 +1,6 @@
 import akka.actor._
 import akka.routing.RoundRobinRouter
-import akka.util.Duration
-import akka.util.duration_
+//import akka.util.duration._
 
 sealed trait StoryMessage
 case class Process(topic: String, htmlBody: String) extends StoryMessage
@@ -14,21 +13,21 @@ class StoryTokenWorker extends Actor {
 			sender ! Result(topic, tokenizeTopic(topic), stripHtml(htmlBody), tokenizeBody(stripHtml(htmlBody)))
 	}
 
-	def stripHtml(text : String) = { body.replaceAll("""<(?!\/?a(?=>|\s.*>))\/?.*?>""", "") }
+	def stripHtml(text : String) = { text.replaceAll("""<(?!\/?a(?=>|\s.*>))\/?.*?>""", "") }
 
-	def tokenizeTopic(text: String) = { text }
+	def tokenizeTopic(text: String) = { text.split(" ") }
 	
-	def tokenizeBody(text: String) = {text}
+	def tokenizeBody(text: String) = { text.split(" ") }
 }
 
 //Worker bee responsible for taking a tokenize story and matching it against others in the database/memory?
-class StoryMatchingWorker extends Actor {
-	
-}
+/*class StoryMatchingWorker extends Actor {
+	def receive = {}	
+}*/
 
 //First in the pipeline, receives a newly identified story and begins working it
 class StoryMaster(numberOfWorkers: Int, listener: ActorRef) extends Actor {
-	val workerRouter = content.actorOf(Props[StoryTokenWorker].withRouter(RoundRobinRouter(numberOfWorkers)), name = "storyTokenWorkerRouter")
+	val workerRouter = context.actorOf(Props[StoryTokenWorker].withRouter(RoundRobinRouter(numberOfWorkers)), name = "storyTokenWorkerRouter")
 
 	def receive = {
 		case Process =>
@@ -39,5 +38,6 @@ class StoryMaster(numberOfWorkers: Int, listener: ActorRef) extends Actor {
 }
 
 //Class which listens for a story to be fully analyzed and puts the results in the DB
-class Listener extends Actor {
-}
+/*class Listener extends Actor {
+	def receive = {}
+}*/
